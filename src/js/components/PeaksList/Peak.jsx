5f16917be7a7e9ee81734cf2de344ae73b306d56
AlexMaz99 <ola_m10@onet.pl>
2021-03-26 11:04:57 +0100

    changed div to link on peak's view

--- src/js/components/PeaksList/Peak.jsx
+++ src/js/components/PeaksList/Peak.jsx
@@ -4,12 +4,11 @@ import styles from './Peak.module.css';
 
 const Peak = ({ peak }) => {
   return (
-    <div className={styles.card}>
+    <Link className={styles.card} to="/exampleRoute">
       {/* TODO - change /exampleRoute to one peak's view */}
-      <Link className={styles.showMoreButton} to="/exampleRoute" />
       <img className={styles.photo} src={peak.photo} alt="mountain" />
       <div className={styles.peakName}>{peak.name}</div>
-    </div>
+    </Link>
   );
 };
 
