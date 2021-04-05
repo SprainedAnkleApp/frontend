085eca72bda706e0cc4713f32614cfec474d3b23
AlexMaz99 <ola_m10@onet.pl>
2021-04-06 00:35:26 +0200

    SA-67; getting peak's photo from backend

--- src/js/views/Peak/usePeakDetails.js
+++ src/js/views/Peak/usePeakDetails.js
@@ -8,13 +8,9 @@ const usePeakDetails = ({ id }) => {
   const getPeakDetails = () => {
     axios.get(`${process.env.REACT_APP_API_URL}api/public/peaks`, { headers: authHeader() }).then(
       (response) => {
-        console.log(response.data);
-        let peak = Object.values(response.data).find((peak) => parseInt(id) === parseInt(peak.id));
-        // TODO - to remove if photo will be in database
-        peak['photo'] =
-          'https://images.unsplash.com/photo-1562878716-48b7542721e3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80';
-
-        setPeakDetailsData(peak);
+        setPeakDetailsData(
+          Object.values(response.data).find((peak) => parseInt(id) === parseInt(peak.id))
+        );
       },
       (error) => {
         console.log(error);
--- src/js/views/PeaksList/usePeaks.js
+++ src/js/views/PeaksList/usePeaks.js
@@ -8,15 +8,7 @@ const usePeaks = () => {
   const getPeaks = async () => {
     axios.get(`${process.env.REACT_APP_API_URL}api/public/peaks`, { headers: authHeader() }).then(
       (response) => {
-        console.log(response.data);
-        // TODO - to remove if photo will be in database
-        const peaks = (response.data ?? []).map((peak) => {
-          peak['photo'] =
-            'https://images.unsplash.com/photo-1562878716-48b7542721e3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80';
-
-          return peak;
-        });
-        setPeaks(peaks);
+        setPeaks(response.data);
       },
       (error) => {
         console.log(error);
