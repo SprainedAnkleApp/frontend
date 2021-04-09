316665667444099c446513c30f4b030e7888d0f7
server-of-disaster <bartoszkaszuba1@gmail.com>
2021-04-09 22:38:17 +0200

    sa-20 mocked header

---
+++ src/js/components/common/Header/Header.jsx
@@ -0,0 +1,7 @@
+import styles from './Header.module.css';
+
+const Header = (state) => {
+  return <div className={styles.header}>Raz dwa trzy</div>;
+};
+
+export default Header;
---
+++ src/js/views/Home/Home.jsx
@@ -0,0 +1,7 @@
+import { Header } from '../../components/common/Header';
+
+const Home = () => {
+  return <Header />;
+};
+
+export default Home;
