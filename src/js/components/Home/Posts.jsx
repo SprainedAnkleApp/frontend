03e56b31df22a548b8907c0e205a68eb8748458b
server-of-disaster <bartoszkaszuba1@gmail.com>
2021-04-12 23:08:17 +0200

    CI/CD warning fix

--- src/js/components/Home/Posts.jsx
+++ src/js/components/Home/Posts.jsx
@@ -1,37 +1,15 @@
 import styles from './Posts.module.css';
 import { Post } from '.';
 import { useState, useEffect } from 'react';
-import Image from '../../../images/mountain.jpg';
 import { getPosts } from '../../API/wall/methods';
 
 const Posts = ({ user }) => {
-  const mockPosts = [
-    {
-      url: user.photoUrl,
-      userName: user.userName,
-      content: 'post1',
-      timestamp: 'mock',
-      liked: 10,
-      comments: 10,
-      watch: 10,
-    },
-    {
-      url: user.photoUrl,
-      userName: user.userName,
-      type: 'photo',
-      content: Image,
-      timestamp: 'mock',
-      liked: 10,
-      comments: 10,
-      watch: 10,
-    },
-  ];
-  const [posts, setPosts] = useState(mockPosts);
+  const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     const posts = getPosts();
     posts.then((data) => {
-      setPosts(mockPosts.concat(data));
+      setPosts(data);
     });
   }, []);
 
