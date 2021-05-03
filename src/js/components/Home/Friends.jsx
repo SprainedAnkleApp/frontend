abde071c98d0e43fbba7d957534ff1d0c1216c90
AlexMaz99 <ola_m10@onet.pl>
2021-05-03 10:02:46 +0200

    SA-89; corrected fetching friends

--- src/js/components/Home/Friends.jsx
+++ src/js/components/Home/Friends.jsx
@@ -3,28 +3,38 @@ import FriendInfo from './FriendInfo';
 import { useEffect, useState } from 'react';
 import { getFriends } from '../../API/friends/methods';
 
+const toFriendInfoComponent = (friend) => {
+  return (
+    <FriendInfo
+      key={`friend_${friend.id}`}
+      id={friend.id}
+      name={friend.firstName + ' ' + friend.lastName}
+      url={friend.profilePhoto}
+      className={styles.friend}
+    />
+  );
+};
+
 const Friends = ({ searchTerm }) => {
   const [searchResult, setSearchResult] = useState([]);
+  const [friends, setFriends] = useState([]);
 
   useEffect(() => {
-    getFriends().then((friends) => {
-      const result = friends
-        .filter(
-          (friend) =>
-            friend.firstName.toLowerCase().includes(searchTerm) ||
-            friend.lastName.toLowerCase().includes(searchTerm)
-        )
-        .map((friend) => (
-          <FriendInfo
-            key={`friend_${friend.id}`}
-            id={friend.id}
-            name={friend.firstName + ' ' + friend.lastName}
-            url={friend.profilePhoto}
-            className={styles.friend}
-          />
-        ));
-      setSearchResult(result);
+    getFriends().then((result) => {
+      setFriends(result);
+      setSearchResult(result.map(toFriendInfoComponent));
     });
+  }, []);
+
+  useEffect(() => {
+    const result = friends
+      .filter(
+        (friend) =>
+          friend.firstName.toLowerCase().includes(searchTerm) ||
+          friend.lastName.toLowerCase().includes(searchTerm)
+      )
+      .map(toFriendInfoComponent);
+    setSearchResult(result);
   }, [searchTerm]);
 
   return (
