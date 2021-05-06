701cbc8f0a6e6373e44dba25c0777f1128583189
AlexMaz99 <ola_m10@onet.pl>
2021-05-06 18:09:43 +0200

    added peak's rating

--- src/js/components/PeaksList/Peak.jsx
+++ src/js/components/PeaksList/Peak.jsx
@@ -1,13 +1,21 @@
 import { Link } from 'react-router-dom';
 
 import styles from './Peak.module.css';
+import { AiFillStar } from 'react-icons/ai';
 
 const Peak = ({ peak }) => {
   return (
     <Link className={styles.card} to={`/peaks/${peak.id}`}>
       <img className={styles.photo} src={peak.photo} alt="mountain" />
-      <div className={styles.peakName}>{peak.name}</div>
-      <div className={styles.peakHeight}>{peak.height} m n.p.m.</div>
+      <div className={styles.peakInformation}>
+        <div>
+          <div className={styles.peakName}>{peak.name}</div>
+          <div className={styles.peakHeight}>{peak.height} m n.p.m.</div>
+        </div>
+        <div className={styles.rating}>
+          <AiFillStar className={styles.star} /> <p className={styles.ratingText}>4.9/5</p>
+        </div>
+      </div>
     </Link>
   );
 };
