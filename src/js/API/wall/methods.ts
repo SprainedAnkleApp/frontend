b9130c2c2b634dd6fe32cddea7f0219d086d9cb6
server-of-disaster <bartoszkaszuba1@gmail.com>
2021-05-11 00:26:00 +0200

    Added posts infinite scroll and pagination hook

--- src/js/API/wall/methods.ts
+++ src/js/API/wall/methods.ts
@@ -12,3 +12,18 @@ export const getPosts = async (): Promise<Post[]> => {
     throw error;
   }
 };
+
+export const getPostsPaginated = (pageSize: number) => async (
+  page: number
+): Promise<{ pages: number; data: Post[] }> => {
+  try {
+    const pagePostsUrl = getPostsUrl() + `?page=${page}&size=${pageSize}`;
+    const response = await axios.get(pagePostsUrl, {
+      headers: authHeader(),
+    });
+    return response.data ?? [];
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
--- src/js/API/wall/urls.ts
+++ src/js/API/wall/urls.ts
@@ -1,2 +1,6 @@
+// export const getPostsUrl = (): string =>
+//   `${process.env.REACT_APP_API_URL}api/public/wallitems`;
+
+// TODO add real paginated post api from backend
 export const getPostsUrl = (): string =>
-  `${process.env.REACT_APP_API_URL}api/public/wallitems`;
+  'https://api.instantwebtools.net/v1/passenger';
