600d7413691debf8c4578870873d0ad6f738ca7f
Konrad Dębiec <konradd@tutanota.com>
2021-05-17 10:10:21 +0200

    Removed: Unused imports

--- src/js/components/Home/Profile.tsx
+++ src/js/components/Home/Profile.tsx
@@ -1,9 +1,4 @@
-import styles from './Posts.module.css';
-import { Post } from '.';
-import { getPostsPaginated } from '../../API/wall/methods';
-import { Post as PostType } from '../../models/interfaces';
 import React from 'react';
-import { Card } from '../common';
 import { ProfileUserCard, ProfileNavBar } from './Profile/index';
 
 export type ProfileProps = {
