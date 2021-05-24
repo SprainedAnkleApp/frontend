e1fc7894b684e6dc5a5343b88a65d0882ab750ac
AlexMaz99 <ola_m10@onet.pl>
2021-05-24 15:06:05 +0200

    added feedback about completion of a peak

--- src/js/components/PeaksList/Peak.tsx
+++ src/js/components/PeaksList/Peak.tsx
@@ -3,7 +3,7 @@ import cx from 'classnames';
 import styles from './Peak.module.css';
 import { Peak as PeakType } from '../../models/interfaces';
 import React from 'react';
-import { AiFillStar } from 'react-icons/ai';
+import { TiTick, TiTimes } from 'react-icons/ti';
 
 export type PeakProps = {
   peak: PeakType;
@@ -20,10 +20,14 @@ const Peak = ({ peak, redirectTo, className }: PeakProps) => {
           <div className={styles.peakName}>{peak.name}</div>
           <div className={styles.peakHeight}>{peak.height} m n.p.m.</div>
         </div>
-        <div className={styles.rating}>
-          <AiFillStar className={styles.star} />{' '}
-          <p className={styles.ratingText}>4.9/5</p>
-        </div>
+      </div>
+      <div className={styles.reachedInfo}>
+        {peak.completed ? (
+          <TiTick className={styles.reachedIcon} />
+        ) : (
+          <TiTimes className={styles.reachedIcon} />
+        )}
+        <p className={styles.reachedText}>Reached</p>
       </div>
     </Link>
   );
