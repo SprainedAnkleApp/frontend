f3406bf33175682d4b963f23fa78f3ee742df4ce
AndrzejPodo <andrzejpodobinski2@gmail.com>
2021-05-31 22:59:21 +0200

    move reach button

--- src/js/components/PeaksList/Peak.tsx
+++ src/js/components/PeaksList/Peak.tsx
@@ -2,8 +2,13 @@ import { Link } from 'react-router-dom';
 import cx from 'classnames';
 import styles from './Peak.module.css';
 import { Peak as PeakType } from '../../models/interfaces';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { SubmitButton, Card } from '../common';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { completeThePeak } from '../../API/peaks/methods';
+import Popup from 'reactjs-popup';
 import { TiTick, TiTimes } from 'react-icons/ti';
+import Error from '../common/Error';
 
 export type PeakProps = {
   peak: PeakType;
@@ -13,6 +18,11 @@ export type PeakProps = {
   peakDetails?: boolean;
 };
 
+type FormValues = {
+  minutes: number;
+  hours: number;
+};
+
 const Peak = ({
   peak,
   redirectTo,
@@ -20,6 +30,22 @@ const Peak = ({
   setShowForm,
   peakDetails = false,
 }: PeakProps) => {
+  const [submitError, setSubmitError] = useState<string | null>(null);
+  const [completed, setCompleted] = useState<boolean>(peak.completed);
+  const { register, errors, handleSubmit } = useForm<FormValues>();
+
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    const promise = completeThePeak(peak.id, data.hours * 60 + data.minutes);
+
+    promise
+      .then(() => {
+        setCompleted(true);
+      })
+      .catch(() => {
+        setSubmitError('Wystąpił błąd');
+      });
+  };
+
   return (
     <div className={cx(styles.card, className)}>
       <Link className={styles.redirectLink} to={redirectTo}>
@@ -34,20 +60,101 @@ const Peak = ({
       <div
         className={cx(
           styles.reachedInfo,
-          peak.completed
+          completed
             ? styles.peakReachedInfo
             : peakDetails && styles.peakNotReachedInfo
         )}
-        onClick={peak.completed ? void 0 : setShowForm}
+        onClick={completed ? void 0 : setShowForm}
       >
-        {peak.completed ? (
+        {completed ? (
           <TiTick className={styles.reachedIcon} />
         ) : (
           <TiTimes className={styles.reachedIcon} />
         )}
-        <p className={styles.reachedText}>
-          {peak.completed ? 'Reached' : 'Reach'}
-        </p>
+
+        <Popup
+          trigger={
+            <p className={styles.reachedText}>
+              {completed ? 'Reached' : 'Reach'}
+            </p>
+          }
+          modal
+          nested
+        >
+          {(close: () => void, isOpen: boolean) => {
+            useEffect(() => {
+              if (!isOpen) return;
+              const rootDiv = document.getElementById('root');
+              if (!rootDiv) return;
+              rootDiv.style.filter = 'blur(2px)';
+
+              return () => {
+                if (!isOpen) return;
+                const rootDiv = document.getElementById('root');
+                if (!rootDiv) return;
+                rootDiv.style.filter = 'none';
+              };
+            }, [isOpen]);
+            return (
+              <Card.Card>
+                <form onSubmit={(e) => e.preventDefault()}>
+                  <label className={styles.label}>
+                    {'Wprowadz czas ukończenia'}
+                  </label>
+                  <div className={styles.timeInputContainer}>
+                    <input
+                      className={styles.timeInput}
+                      type={'number'}
+                      name={'hours'}
+                      min={'0'}
+                      max={'100'}
+                      defaultValue={0}
+                      ref={register({
+                        required: 'Pole wymagane',
+                        validate: {
+                          nonNegative: (v) => parseInt(v) >= 0,
+                        },
+                      })}
+                    />{' '}
+                    :
+                    <input
+                      className={styles.timeInput}
+                      type={'number'}
+                      name={'minutes'}
+                      min={'0'}
+                      max={'59'}
+                      defaultValue={0}
+                      ref={register({
+                        required: 'Pole wymagane',
+                        validate: {
+                          nonNegative: (v) => parseInt(v) >= 0,
+                        },
+                      })}
+                    />
+                    {errors.hours && errors.hours.message && (
+                      <Error text={errors.hours.message} />
+                    )}
+                    {errors.minutes &&
+                      !errors.hours?.message &&
+                      errors.minutes.message && (
+                        <Error text={errors.minutes.message} />
+                      )}
+                  </div>
+                  <SubmitButton
+                    onClick={handleSubmit((data) => {
+                      onSubmit(data);
+                      close();
+                    })}
+                    text="Zatwierdz"
+                    className={styles.reachPeakButton}
+                  />
+
+                  {submitError && <Error text={submitError} />}
+                </form>
+              </Card.Card>
+            );
+          }}
+        </Popup>
       </div>
     </div>
   );
