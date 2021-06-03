170100ee74a7b79c2de3b1f719d83b650ab56ebc
Konrad Dębiec <konradd@tutanota.com>
2021-06-03 22:51:55 +0200

    Removed: Profile mock photo

--- src/js/components/Home/Profile/ProfileUserCard.tsx
+++ src/js/components/Home/Profile/ProfileUserCard.tsx
@@ -1,5 +1,5 @@
-import styles from './ProfileUserCard.module.css';
 import React from 'react';
+import styles from './ProfileUserCard.module.css';
 import { Card } from '../../common';
 
 export type ProfileUserCardProps = {
@@ -17,13 +17,12 @@ const ProfileUserCard = ({
         <div className={styles.upperContainer}>
           <div className={styles.backgroundImg}></div>
           <div className={styles.profileImg}>
-            <img src={profilePhoto} alt="icon" className={styles.photo} />
+            <img src={profilePhoto} className={styles.profileImg} />
           </div>
         </div>
         <div className={styles.bottomContainer}>
           <div className={styles.userNameWrapper}>
             <p className={styles.userName}>{profileUserName}</p>
-            <p className={styles.lvl}>Zdobywca - lvl 14</p>
           </div>
         </div>
       </Card.Card>
