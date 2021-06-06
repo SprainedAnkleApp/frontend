bec60750721cfdeecfd86d40481b1891636ca509
AlexMaz99 <ola_m10@onet.pl>
2021-06-06 16:18:13 +0200

    endpoint for user wall items

--- src/js/API/wall/urls.ts
+++ src/js/API/wall/urls.ts
@@ -4,6 +4,13 @@ export const getPostsUrl = (): string =>
 export const getPaginatedPostsUrl = (page: number, pageSize: number): string =>
   getPostsUrl() + `?pageNumber=${page}&pageSize=${pageSize}`;
 
+export const getUserPaginatedPostsUrl = (
+  page: number,
+  pageSize: number,
+  userId: string
+): string =>
+  getPostsUrl() + `/${userId}?pageNumber=${page}&pageSize=${pageSize}`;
+
 export const createNewPostUrl = (): string =>
   `${process.env.REACT_APP_API_URL}api/public/posts/post`;
 
