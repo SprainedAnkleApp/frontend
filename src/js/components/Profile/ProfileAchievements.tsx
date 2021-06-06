c0a30ddd8e58dd4efda25471e64fb00fbb42e3c0
Konrad Dębiec <konradd@tutanota.com>
2021-06-06 15:08:34 +0200

    Show users achivements on profile

--- src/js/components/Profile/ProfileAchievements.tsx
+++ src/js/components/Profile/ProfileAchievements.tsx
@@ -1,15 +1,17 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import styles from './ProfileAchievements.module.css';
 import { Achievements } from '../RightBar/Achievements';
 import { Card } from '../common';
-import { userContext } from '../../contexts/CurrentUser';
 
-const ProfileAchievements = () => {
-  const { user } = useContext(userContext);
+export type ProfileAchievementsProps = {
+  userId: string;
+};
+
+const ProfileAchievements = ({ userId }: ProfileAchievementsProps) => {
   return (
     <div className={styles.achivementsWrapper}>
       <Card.Card>
-        <Achievements userId={user.id.toString()} />
+        <Achievements userId={userId} />
       </Card.Card>
     </div>
   );
--- src/js/views/Profile/Profile.tsx
+++ src/js/views/Profile/Profile.tsx
@@ -49,7 +49,7 @@ const Profile = ({ className }: ProfileProps) => {
             scrollId="postsScroll"
           ></Posts>
         )}
-        {state === 'achievements' && <ProfileAchievements />}
+        {state === 'achievements' && <ProfileAchievements userId={userId} />}
         {state === 'friends' && (
           <ProfileFriends
             key={userId}
