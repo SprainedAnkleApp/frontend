3b294a431a892795a275f5af250a17c3e10c80f5
Konrad Dębiec <konradd@tutanota.com>
2021-06-10 12:43:18 +0200

    Polished Post style

--- src/js/components/common/Card.tsx
+++ src/js/components/common/Card.tsx
@@ -4,6 +4,7 @@ import styles from './Card.module.css';
 import React from 'react';
 import Icon from './Icon';
 import { User } from '../../models/interfaces';
+import { useHistory } from 'react-router';
 
 export type CardProps = {
   className?: string;
@@ -35,16 +36,28 @@ const CardHeader = ({
   user,
   className,
 }: CardHeaderProps) => {
+  const history = useHistory();
+  const redirectToUser = (userId: string) => {
+    history.push({
+      pathname: `/profile/${userId}`,
+    });
+  };
+
   return (
     <div className={cx(styles.header, className)}>
-      <Icon url={user?.profilePhoto} variant="s" />
-      <div className={styles.nameWithTime}>
-        <span className={styles.userName}>
-          {user?.firstName + ' ' + user?.lastName}
-        </span>
-        <span className={cx(styles.time, { [styles.active]: active })}>
-          {active ? 'Active' : timestamp}
-        </span>
+      <div
+        className={styles.headerUser}
+        onClick={() => redirectToUser(user.id.toString())}
+      >
+        <Icon url={user?.profilePhoto} variant="s" />
+        <div className={styles.nameWithTime}>
+          <span className={styles.userName}>
+            {user?.firstName + ' ' + user?.lastName}
+          </span>
+          <span className={cx(styles.time, { [styles.active]: active })}>
+            {active ? 'Active' : timestamp}
+          </span>
+        </div>
       </div>
       {rightPart && <div className={styles.onRight}>{rightPart}</div>}
     </div>
